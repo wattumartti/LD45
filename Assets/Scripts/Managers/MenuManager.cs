0118a784d661659efd990be1ed3d1fd326136542
Wattumartti <waltteri.hyvonen@gmail.com>
2019-10-05 22:05:27 +0300

    DOTween, renamed scene, basic movement stuff

---
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,54 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    private static MenuManager _instance = null;
+
+    public static MenuManager Instance
+    {
+        get
+        {
+            return _instance;
+        }
+        private set
+        {
+            if (_instance != null && _instance != value)
+            {
+                Destroy(_instance.gameObject);
+            }
+
+            _instance = value;
+        }
+    }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void StartGame()
+    {
+#if UNITY_EDITOR
+        Debug.Log("start game");
+#endif
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        Debug.Log("quit game");
+#endif
+        Application.Quit();
+    }
+
+    public void TweenOut(GameObject objectToTween)
+    {
+        CanvasGroup canvas = objectToTween.GetComponent<CanvasGroup>();
+
+        canvas.DOFade(0, 2).OnComplete(() => { objectToTween.SetActive(false); });
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D body = null;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private SpriteRenderer playerSprite = null;
+
+    [Header("Ground Checking")]
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] float groundRayCastDistance;
+
+    private float horizontalMovement = 0;
+    private bool useJump = false;
+
+    public bool isGrounded = false;
+
+    private void Update()
+    {
+        horizontalMovement = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonUp("Vertical"))
+        {
+            useJump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        UpdateGrounded();
+
+        if (horizontalMovement != 0)
+        {
+            Vector3 movement = new Vector3(horizontalMovement, 0.0f, 0.0f);
+            body.AddForce(movement * speed);
+        }
+        
+        if (useJump && isGrounded)
+        {
+            Vector3 jumpMovement = new Vector3(0.0f, jumpForce, 0.0f);
+            body.AddForce(jumpMovement, ForceMode2D.Impulse);
+            useJump = false;
+        }
+    }
+
+    private void UpdateGrounded()
+    {
+        if (Physics2D.Raycast(playerSprite.transform.position - new Vector3(0, playerSprite.bounds.extents.y, 0), -transform.up, groundRayCastDistance, groundLayer))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+}
